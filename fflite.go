4cff2deefa32f35c557e61429d8cba3830239c14
malashin <malashinanton@gmail.com>
2017-02-01 19:02:30 +1000

    Fix index out of range error

--- fflite.go
+++ fflite.go
@@ -13,7 +13,7 @@ import (
 )
 
 // Global variables.
-var version = "v0.1.9"
+var version = "v0.1.10"
 var presets = map[string]string{
 	`^\@crf(\d+)$`: "-an -vcodec libx264 -preset medium -crf ${1} -pix_fmt yuv420p -g 0 -map_metadata -1 -map_chapters -1",
 	`^\@ac(\d+)$`:  "-vn -acodec ac3 -ab ${1}k -map_metadata -1 -map_chapters -1",
@@ -65,7 +65,7 @@ func main() {
 	// Parse all arguments and apply presets if needed.
 	// Arguments surrounded by escaped doublequotes are joined.
 	for i := 0; i < len(args); i++ {
-		if (len(args) >= 2) && (args[i] == "-i") && (strings.HasSuffix(args[i+1], ".txt")) {
+		if (i+1 < len(args)) && (args[i] == "-i") && (strings.HasSuffix(args[i+1], ".txt")) {
 			if batchInputName == "" {
 				batchInputName = args[i+1]
 			} else {
@@ -73,7 +73,7 @@ func main() {
 				os.Exit(1)
 			}
 		}
-		if (len(args) >= 2) && (args[i] == "-i") && (firstInput == "") {
+		if (i+1 < len(args)) && (args[i] == "-i") && (firstInput == "") {
 			firstInput = args[i+1]
 		}
 		if !appendArgs {
